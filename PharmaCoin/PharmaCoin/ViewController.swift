24dd76a165bbe2ce6e8c3043d2ebe50db1525270
Collin Cao <collin@airbears2-10-142-64-39.airbears2.1918.berkeley.edu>
2019-04-29 00:55:52 -0700

    fix submodule

---
+++ PharmaCoin/PharmaCoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PharmaCoin
+//
+//  Created by Collin Cao on 4/28/19.
+//  Copyright © 2019 Collin Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
